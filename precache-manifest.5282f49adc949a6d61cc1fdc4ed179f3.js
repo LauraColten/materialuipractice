c9649ad8b2582c92666328f11ff9306d90ca7694
Laura Colten <lauracolten5@gmail.com>
2019-09-23 22:59:44 -0500

    Updates

---
+++ precache-manifest.5282f49adc949a6d61cc1fdc4ed179f3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1deb460c39082a81273238e93b852b8f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c8333db7faab1f6f35c4",
+    "url": "/static/css/main.773b4609.chunk.css"
+  },
+  {
+    "revision": "98232290df804491c668",
+    "url": "/static/js/2.dbb5b7cf.chunk.js"
+  },
+  {
+    "revision": "c8333db7faab1f6f35c4",
+    "url": "/static/js/main.22e45b6d.chunk.js"
+  },
+  {
+    "revision": "869e79153416ac8ff584",
+    "url": "/static/js/runtime-main.ed506474.js"
+  }
+]);
